06e245a1b93bfeea579bf601e3c56f8866e69f7d
phihash <83079502+phihash@users.noreply.github.com>
2023-05-31 03:53:31 +0900

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,13 +1,12 @@
 import Header from './header';
 import Footer from './footer';
-import { Html } from 'next/document';
 
 export default function Layout({ children }) {
   return (
-    <Html lang="ja">
+    <>
       <Header />
       <main>{children}</main>
       <Footer />
-    </Html>
+    </>
   );
 }
